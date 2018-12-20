6550f0a50d9f29480706b06b8aff5587fdec3c54
David Mateus <damateus964@misena.edu.co>
2018-12-20 18:20:03 -0500

    Version 1.1.1 Trabajando en el apartado vehiculos

--- OMB_Base_de_datos/Frames/Listado_Vehiculos.Designer.cs
+++ OMB_Base_de_datos/Frames/Listado_Vehiculos.Designer.cs
@@ -29,7 +29,20 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Listado_Vehiculos));
             this.FormaApp = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.ListadoVeh = new Bunifu.Framework.UI.BunifuCustomDataGrid();
+            this.PdfVeh = new System.Windows.Forms.Button();
+            this.IconoBuscar = new System.Windows.Forms.PictureBox();
+            this.Cabeza = new System.Windows.Forms.Panel();
+            this.Buscar = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            ((System.ComponentModel.ISupportInitialize)(this.ListadoVeh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconoBuscar)).BeginInit();
+            this.Cabeza.SuspendLayout();
             this.SuspendLayout();
             // 
             // FormaApp
@@ -37,14 +50,125 @@
             this.FormaApp.ElipseRadius = 5;
             this.FormaApp.TargetControl = this;
             // 
+            // ListadoVeh
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ListadoVeh.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.ListadoVeh.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListadoVeh.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.ListadoVeh.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ListadoVeh.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(37)))), ((int)(((byte)(66)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ListadoVeh.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.ListadoVeh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.ListadoVeh.DefaultCellStyle = dataGridViewCellStyle3;
+            this.ListadoVeh.DoubleBuffered = true;
+            this.ListadoVeh.EnableHeadersVisualStyles = false;
+            this.ListadoVeh.HeaderBgColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(37)))), ((int)(((byte)(66)))));
+            this.ListadoVeh.HeaderForeColor = System.Drawing.SystemColors.Control;
+            this.ListadoVeh.Location = new System.Drawing.Point(6, 153);
+            this.ListadoVeh.Name = "ListadoVeh";
+            this.ListadoVeh.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ListadoVeh.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.ListadoVeh.Size = new System.Drawing.Size(1276, 569);
+            this.ListadoVeh.TabIndex = 7;
+            // 
+            // PdfVeh
+            // 
+            this.PdfVeh.FlatAppearance.BorderSize = 0;
+            this.PdfVeh.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SteelBlue;
+            this.PdfVeh.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.PdfVeh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PdfVeh.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PdfVeh.Image = ((System.Drawing.Image)(resources.GetObject("PdfVeh.Image")));
+            this.PdfVeh.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PdfVeh.Location = new System.Drawing.Point(7, 102);
+            this.PdfVeh.Name = "PdfVeh";
+            this.PdfVeh.Size = new System.Drawing.Size(282, 44);
+            this.PdfVeh.TabIndex = 3;
+            this.PdfVeh.Text = "Generar archivo PDF";
+            this.PdfVeh.UseVisualStyleBackColor = true;
+            // 
+            // IconoBuscar
+            // 
+            this.IconoBuscar.Image = ((System.Drawing.Image)(resources.GetObject("IconoBuscar.Image")));
+            this.IconoBuscar.Location = new System.Drawing.Point(406, 102);
+            this.IconoBuscar.Name = "IconoBuscar";
+            this.IconoBuscar.Size = new System.Drawing.Size(41, 44);
+            this.IconoBuscar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.IconoBuscar.TabIndex = 1;
+            this.IconoBuscar.TabStop = false;
+            // 
+            // Cabeza
+            // 
+            this.Cabeza.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Cabeza.Controls.Add(this.PdfVeh);
+            this.Cabeza.Controls.Add(this.IconoBuscar);
+            this.Cabeza.Controls.Add(this.Buscar);
+            this.Cabeza.Location = new System.Drawing.Point(-1, -2);
+            this.Cabeza.Name = "Cabeza";
+            this.Cabeza.Size = new System.Drawing.Size(1283, 149);
+            this.Cabeza.TabIndex = 6;
+            // 
+            // Buscar
+            // 
+            this.Buscar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Buscar.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Buscar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Buscar.ForeColor = System.Drawing.Color.DimGray;
+            this.Buscar.HintForeColor = System.Drawing.Color.Empty;
+            this.Buscar.HintText = "";
+            this.Buscar.isPassword = false;
+            this.Buscar.LineFocusedColor = System.Drawing.Color.DodgerBlue;
+            this.Buscar.LineIdleColor = System.Drawing.Color.Gray;
+            this.Buscar.LineMouseHoverColor = System.Drawing.Color.SteelBlue;
+            this.Buscar.LineThickness = 4;
+            this.Buscar.Location = new System.Drawing.Point(454, 102);
+            this.Buscar.Margin = new System.Windows.Forms.Padding(4);
+            this.Buscar.Name = "Buscar";
+            this.Buscar.Size = new System.Drawing.Size(825, 44);
+            this.Buscar.TabIndex = 0;
+            this.Buscar.Text = "Buscar...";
+            this.Buscar.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
             // Listado_Vehiculos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.ListadoVeh);
+            this.Controls.Add(this.Cabeza);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Listado_Vehiculos";
             this.Text = "Listado_Vehiculos";
+            ((System.ComponentModel.ISupportInitialize)(this.ListadoVeh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconoBuscar)).EndInit();
+            this.Cabeza.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -52,5 +176,10 @@
         #endregion
 
         private Bunifu.Framework.UI.BunifuElipse FormaApp;
+        private Bunifu.Framework.UI.BunifuCustomDataGrid ListadoVeh;
+        private System.Windows.Forms.Panel Cabeza;
+        private System.Windows.Forms.Button PdfVeh;
+        private System.Windows.Forms.PictureBox IconoBuscar;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox Buscar;
     }
 }
